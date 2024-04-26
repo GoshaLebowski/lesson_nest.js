046adc8812fda559ccbf4d7b347483488e16438c
GoshaLebowski <sirinkinegor@gmail.com>
2024-04-26 11:08:29 +0500

    Push изменений

---
+++ backend/src/moks/index.ts
@@ -0,0 +1,42 @@
+export const users = [
+    {
+        "id": 56789,
+        "username": "example123",
+        "email": "example123@example.com",
+        "first_name": "Sophia",
+        "last_name": "Martinez",
+        "age": 32,
+        "city": "Madrid",
+        "country": "Spain"
+    },
+    {
+        "id": 45678,
+        "username": "user456",
+        "email": "user456@example.com",
+        "first_name": "Michael",
+        "last_name": "Brown",
+        "age": 28,
+        "city": "Berlin",
+        "country": "Germany"
+    },
+    {
+        "id": 34567,
+        "username": "test_user",
+        "email": "test@example.com",
+        "first_name": "Emma",
+        "last_name": "Smith",
+        "age": 35,
+        "city": "London",
+        "country": "UK"
+    },
+    {
+        "id": 23456,
+        "username": "user234",
+        "email": "user234@example.com",
+        "first_name": "Alice",
+        "last_name": "Johnson",
+        "age": 25,
+        "city": "Los Angeles",
+        "country": "USA"
+    }
+]
